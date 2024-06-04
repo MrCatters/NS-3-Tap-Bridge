84e17ef34e0e85c4788d228690e3788062f71d3a
unknown <73203569+MrCatters@users.noreply.github.com>
2024-06-04 14:59:22 -0400

    added tap config to install

--- install.sh
+++ install.sh
@@ -6,6 +6,8 @@ wget https://www.nsnam.org/releases/ns-allinone-3.42.tar.bz2
 tar xfj ns-allinone-3.42.tar.bz2
 cd ns-allinone-3.42/ns-3.42
 
+cp tap-wifi-virtual-machine.cc scratch/tap-vm.cc
+
 echo -e "\n\n compiling NS3 using all available CPUs  ... \n"
 ./ns3 configure --build-profile=debug --enable-sudo --enable-examples --enable-tests
 ./ns3 build --jobs $(nproc)
